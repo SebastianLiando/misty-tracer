6dfd8b306d912f0faacc4727ef49ab4954f47e6f
Sebastian <sebastianliando@gmail.com>
2022-02-28 16:19:58 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/zetzaus/misty_tracer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zetzaus.misty_tracer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const mainIndigo = Color(0xFF353982);
+const error = Color(0xFFFF7660);
+
+const pendingChip = Color(0xFFFFCE50);
+const idleChip = Color(0xFFCAE7C1);
+const engagingChip = Color(0xFF03D8AE);
+const capturingChip = Color(0xFFBBDEFB);
+const verifyingChip = Color(0xFF79D2E6);
+const acceptChip = Color(0xFF71D05E);
